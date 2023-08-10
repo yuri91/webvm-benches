cc1612dfae4a0decfc430ebe8c51f8acefde476c
Yuri Iozzelli <y.iozzelli@gmail.com>
2023-08-10 11:55:56 +0200

    improve bench scripts

--- benches/bench.sh
+++ benches/bench.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-cd benches32
+set -e
 
-for f in ./*.native; do
-	echo -n "${f}: "
-	/bin/time -f "%e" ${f} > /dev/null 2> /tmp/dump
-	cat /tmp/dump | tr '\n' ' '
-	/bin/time -f "%e" ${f} > /dev/null 2> /tmp/dump
-	cat /tmp/dump | tr '\n' ' '
-	/bin/time -f "%e" ${f} > /dev/null 2> /tmp/dump
-	cat /tmp/dump | tr '\n' ' '
-	/bin/time -f "%e" ${f} > /dev/null 2> /tmp/dump
-	cat /tmp/dump | tr '\n' ' '
-	/bin/time -f "%e" ${f} > /dev/null 2> /tmp/dump
-	cat /tmp/dump | tr '\n' ' '
-	echo
+MY_DIR=`dirname "$(readlink -f "$0")"`
+
+outfile=$1
+nsamples=$2
+
+rm -f ${outfile}
+for f in ${MY_DIR}/benches32/*.native; do
+	echo -n "$(basename -- ${f}): " | tee -a ${outfile}
+	for (( i=0; i<=$nsamples; i++ ))
+	do
+		/bin/time -f "%e" ${f} > /dev/null 2> /tmp/dump
+		cat /tmp/dump | tr '\n' ' ' | tee -a ${outfile}
+	done
+	echo | tee -a ${outfile}
 done
--- benches/compare.py
+++ benches/compare.py
@@ -9,16 +9,16 @@ def collect(fname):
         for l in f.readlines():
             split = l.split(":")
             name = split[0]
-            samples = split[1][1:].split(" ")
+            samples = split[1].strip(" \n").split(" ")
             samples = np.array([float(s) for s in samples])
             benches[name] = samples
     return benches
 
-native = collect(sys.argv[1])
-cheerpx = collect(sys.argv[2])
+baseline = collect(sys.argv[1])
+target = collect(sys.argv[2])
 
 print(f"{sys.argv[2]}/{sys.argv[1]}")
-for b in native:
-    avgn = np.mean(native[b])
-    avgc = np.mean(cheerpx[b])
-    print(f"{b}: {avgc/avgn}")
+for b in baseline:
+    avgbaseline = np.mean(baseline[b])
+    avgtarget = np.mean(target[b])
+    print(f"{b}: {avgtarget/avgbaseline}")
